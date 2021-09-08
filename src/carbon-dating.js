e7a82995216f6f495c8764ad2f25d9d6a5ab11c8
Nick Simmons <nicksimmons@MacBook-Pro-Nick-2.local>
2021-09-08 17:21:15 +0500

    done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(string) {
+  if ((typeof (string) !== 'string') ||
+    isNaN(parseFloat(string)) ||
+    (!isNaN(string) && (+string <= 0 || +string > 15))) {
+    return false
+  }
+  return Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(string)) /
+    (0.693 / HALF_LIFE_PERIOD))
 }
